23c537dc18b2127ca86f44cd3cfe278a63e675c6
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2023-03-02 17:23:26 +0800

    new file:   CMakeLists.txt 	new file:   CMakePresets.json 	new file:   opencv/CMakeLists.txt 	new file:   opencv/opencv.cpp 	new file:   opencv/opencv.h

---
+++ opencv/opencv.cpp
@@ -0,0 +1,20 @@
+﻿// opencv.cpp: 定义应用程序的入口点。
+//
+
+#include "opencv.h"
+
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp> 
+using namespace cv;
+
+int main()
+{
+    
+    Mat img = imread("C:\\Users\\33298\\OneDrive\\Pictures\\332969.jpg");
+    namedWindow("测试opencv");
+    imshow("测试opencv", img);
+    waitKey(1000);
+    
+    return 0;
+    
+}
